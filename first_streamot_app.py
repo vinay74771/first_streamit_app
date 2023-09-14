c8b4467a49091600057d890bba471cec41169e9b
vinay74771 <144652507+vinay74771@users.noreply.github.com>
2023-09-14 15:09:45 +0530

    Update first_streamot_app.py

--- first_streamot_app.py
+++ first_streamot_app.py
@@ -21,6 +21,8 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 
 
 import requests
