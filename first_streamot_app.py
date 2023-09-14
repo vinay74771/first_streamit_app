4868c90aef647aa7fbda4ad32bbddf1be5e96977
vinay74771 <144652507+vinay74771@users.noreply.github.com>
2023-09-14 20:20:04 +0530

    Update first_streamot_app.py

--- first_streamot_app.py
+++ first_streamot_app.py
@@ -33,5 +33,5 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
 
-
+import snowflake.connector
 
