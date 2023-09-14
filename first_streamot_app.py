a2c5b3210d20235660946760b751f1061d24b276
vinay74771 <144652507+vinay74771@users.noreply.github.com>
2023-09-14 14:55:05 +0530

    Update first_streamot_app.py

--- first_streamot_app.py
+++ first_streamot_app.py
@@ -26,4 +26,10 @@ streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+# write your own comment -what does the next line do? 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# write your own comment - what does this do?
+streamlit.dataframe(fruityvice_normalized)
+
+
 
