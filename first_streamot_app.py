9996f1e1bed83538385a144d7654b6ca3c8b2145
vinay74771 <144652507+vinay74771@users.noreply.github.com>
2023-09-14 11:03:42 +0530

    Update first_streamot_app.py

--- first_streamot_app.py
+++ first_streamot_app.py
@@ -5,7 +5,7 @@ streamlit.header('Breakfast Favorites')
 streamlit.text( '🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text( '🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞 Avocado Toast")
+streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
