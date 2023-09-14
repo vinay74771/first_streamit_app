0129c0616af4a2250cb86080cf9c7903055d0f27
vinay74771 <144652507+vinay74771@users.noreply.github.com>
2023-09-14 11:06:22 +0530

    Update first_streamot_app.py
    pandas dataframe called my_fruit_list

--- first_streamot_app.py
+++ first_streamot_app.py
@@ -9,3 +9,6 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
